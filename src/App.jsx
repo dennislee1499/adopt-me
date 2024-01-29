751d19f76c5dbfca0bce74cb91ed60b72cceaa45
Dennis Lee <dennislee1499@gmail.com>
2024-01-29 00:02:31 -0500

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import Pet from "./Pet.jsx";
-
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Loki" animal="Dog" breed="Pomeranian"></Pet>
-      <Pet name="Hazel" animal="Cat" breed="Tabby"></Pet>
-      <Pet name="Churro" animal="Rabbit" breed="Holland Lop"></Pet>
+      <SearchParams />
     </div>
   );
 };
